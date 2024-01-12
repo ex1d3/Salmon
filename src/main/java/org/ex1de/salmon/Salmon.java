232822108e6a96ce6769d17aa2a6662bf77599e9
артур <okolobaxha@yandex.ru>
2024-01-12 11:05:16 +0700

    project initialized

---
+++ src/main/java/org/ex1de/salmon/Salmon.java
@@ -0,0 +1,17 @@
+package org.ex1de.salmon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Salmon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
